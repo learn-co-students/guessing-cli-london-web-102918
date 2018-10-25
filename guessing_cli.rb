29960cef42a8bcd8e2ef13ca2281754ab438bfdf
Paul-Cl-ark <clark.paul123@gmail.com>
2018-10-25 10:14:25 +0100

    First command line commit

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,16 @@
-"Hello world"
+def run_guessing_game
+  number = rand(1..6).to_s
+  while number
+    puts "Guess a number between 1 and 6."
+    user_input = gets.strip.downcase
+    case user_input
+    when number
+      puts "You guessed the correct number!"
+    when "exit"
+      puts "Goodbye!"
+      break
+    else
+      puts "The computer guessed #{number}."
+    end
+  end
+end 
